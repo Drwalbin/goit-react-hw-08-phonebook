418c740fdbaa0bbf4fd3aca0533d5c8e074f2acf
Drwalbin <99756243+Drwalbin@users.noreply.github.com>
2023-05-19 20:36:35 +0000

    Deploying to gh-pages from @ Drwalbin/goit-react-hw-08-phonebook@4d4f51d2bd70e52225da7dff30af3dedbdaeb92d 🚀

---
+++ static/js/909.e81a1e3f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[909],{5909:function(e,n,o){o.r(n),o.d(n,{default:function(){return i}});var t="HomePage_container__+8deC",c=o(7022),r=o(184),a=function(){return(0,r.jsx)(c.Z,{className:t,children:(0,r.jsx)("h1",{className:"text-center",children:"Welcome to Phonebook Application \ud83d\udcd6"})})},i=function(){return(0,r.jsx)(a,{})}}}]);
+//# sourceMappingURL=909.e81a1e3f.chunk.js.map
